390d34cbde8ddd09bdeb37f42a404e4ee9c97391
ReginFell <ReginFell@gmail.com>
2018-06-19 22:37:27 +0300

    Extract splash into separate module

---
+++ splash/src/main/java/com/regin/learnbox/splash/presentation/splash/SplashModule.kt
@@ -0,0 +1,8 @@
+package com.regin.learnbox.splash.presentation.splash
+
+import org.koin.android.architecture.ext.viewModel
+import org.koin.dsl.module.applicationContext
+
+val splashModule = applicationContext {
+    viewModel { SplashViewModel(get()) }
+}
